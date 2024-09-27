ac36f6492dc36f8986a5c267ef246815299b0ff7
Ubaid-A <wafaubaid3@gmail.com>
2024-09-27 20:34:10 +0500

    create a new list item

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ function addTask() {
     var taskInput = document.getElementById('taskInput');
     var taskList = document.getElementById('taskList');
 
-    // handle empty task
+// handle empty task
     if (!taskInput.value.trim()) {
         return alert("Todo can't be empty!")
     }
+// Create a new list item
+    var li = document.createElement('li');
+    li.textContent = taskInput.value;
+    
